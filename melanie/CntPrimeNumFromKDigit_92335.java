e124697800138c3ef211cbfd74c16fb35629d7c5
melanieing <melanie0617@gmail.com>
2024-07-22 16:53:35 +0900

    92335. k진수에서 소수 개수 구하기

--- melanie/CntPrimeNumFromKDigit_92335.java
+++ melanie/CntPrimeNumFromKDigit_92335.java
@@ -5,20 +5,20 @@ public class CntPrimeNumFromKDigit_92335 {
 
         String num = Long.toString(n, k);
         int count = 0;
+
         for (int i = 0; i <= num.length(); i++) {
             for (int j = i + 1; j <= num.length(); j++) {
                 String tmp = num.substring(i, j);
                 if (!tmp.equals("") && isPrime(tmp)) {
                     if (
-                            (i == 0 && j < num.length() && num.charAt(j) == 48) || // 소수 오른쪽에만 0이 있고 왼쪽에는 아무것도 없는 경우
-                                    (j == num.length() && i != 0 && num.charAt(i-1) == 48) || // 소수 왼쪽에만 0이 있고 오른쪽에는 아무것도 없는 경우
-                                    (i == 0 && j == num.length()) || // 소수 양쪽에 아무것도 없는 경우
-                                    (i > 0 && num.charAt(i-1) == 48 && j < num.length() && num.charAt(j) == 48) // 소수 양쪽에 0이 있는 경우
+                        (i == 0 && j < num.length() && num.charAt(j) == 48) || // 소수 오른쪽에만 0이 있고 왼쪽에는 아무것도 없는 경우
+                        (j == num.length() && i != 0 && num.charAt(i-1) == 48) || // 소수 왼쪽에만 0이 있고 오른쪽에는 아무것도 없는 경우
+                        (i == 0 && j == num.length()) || // 소수 양쪽에 아무것도 없는 경우
+                        (i > 0 && num.charAt(i-1) == 48 && j < num.length() && num.charAt(j) == 48) // 소수 양쪽에 0이 있는 경우
                     )
                     {
                         count++;
                     }
-
                 }
             }
         }
@@ -29,7 +29,7 @@ public class CntPrimeNumFromKDigit_92335 {
     public boolean isPrime(String num) { // 1과 자기 자신으로 밖에 나누어 떨어지지 않고 자기 자신의 곱셈의 역수가 없는 수
         if (num.contains("0")) return false; // 각 자릿수에 0을 포함하면 넘어가기
 
-        Long target = Long.parseLong(num);
+        long target = Long.parseLong(num);
         if (target == 1) return false;
         if (target == 2) return true;
 
