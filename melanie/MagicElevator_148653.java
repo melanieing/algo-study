7de8cd411727c8416db949c54aa9aab6af25b9ba
melanieing <melanie0617@gmail.com>
2024-08-03 14:10:17 +0900

    148653. 마법의 엘리베이터 풀이

--- melanie/MagicElevator_148653.java
+++ melanie/MagicElevator_148653.java
@@ -1,49 +1,34 @@
 // 148653. 마법의 엘리베이터
 public class MagicElevator_148653 {
     public static void main(String[] args) {
-        int storey = 80;
+        int storey = 2554;
         MagicElevator_148653 q = new MagicElevator_148653();
         System.out.println(q.solution(storey));
     }
 
     public int solution(int storey) {
-        // 909 => 900 => -1 * 9 + -10 * 0 + -100 * 9 = 0
-        // 909 => 910 => +1 * 1 + -10 * 1 + -100 * 9 = 0
-        // 80 =?
-        int minToTenth = getTenthNum(storey, -1);
-        int maxToTenth = getTenthNum(storey, 1);
+        int answer = 0;
 
-        int minToZero = getNumToZero(storey);
-        int maxToZero = getNumToZero(storey);
+        while (storey != 0) {
+            int first = storey % 10; // 1의 자릿수
+            int tenth = (storey / 10) % 10; // 10의 자릿수
 
-        int totalMinToZero = minToTenth + minToZero;
-        int totalMaxToZero = maxToTenth + maxToZero;
+            if (first > 5) { // 1의 자릿수가 5보다 크면 올리는 게 더 빠름
+                answer += 10 - first; // 10
+                storey += 10;
+            } else if (first == 5) { // 정확히 5면 십의 자릿수롤 보고 판단
+                answer += first;
+                storey += tenth < 5 ? 0 : 10;
+            } else { // 5보다 작으면 내리는 게 더 빠름
+                answer += first;
+            }
 
-        return Math.min(totalMinToZero, totalMaxToZero);
-    }
-
-    private int getNumToZero(int num) {
-        int numToZero = 0;
-        while (num != 0) {
-            int digits = String.valueOf(num).length(); // 4
-            int tmp = (int)(num / Math.pow(10, digits-1)); // 2550 / 1000
-            numToZero += tmp;
-            num -= tmp * Math.pow(10, digits-1);
+            storey /= 10;
         }
-        return numToZero;
+
+        return answer;
+
     }
 
-    private int getTenthNum(int num, int direction) {
-        int numToTenth = 0;
 
-        if (num % 10 == 0) return num;
-
-        do {
-            if (direction == 1) num++;
-            else num--;
-            numToTenth++;
-        } while (num % 10 != 0);
-
-        return numToTenth;
-    }
 }
