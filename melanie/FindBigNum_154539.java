c8760c075600e1dc68628b60836ed27ca083a694
melanieing <melanie0617@gmail.com>
2024-08-08 23:46:22 +0900

    154539. 뒤에 있는 큰 수 찾기 풀이

--- melanie/FindBigNum_154539.java
+++ melanie/FindBigNum_154539.java
@@ -1,21 +1,59 @@
 import java.util.Arrays;
+import java.util.Stack;
 
-/**
- * 정수로 이루어진 배열 numbers가 있습니다. 배열의 각 원소들에 대해 자신보다 뒤에 있는 숫자 중에서 자신보다 크면서 가장 가까이 있는 수를 뒷 큰수라고 합니다.
- * 정수 배열 numbers가 매개변수로 주어질 때, 모든 원소에 대한 뒷 큰수들을 차례로 담은 배열을 return 하도록 solution 함수를 완성해주세요.
- * 단, 뒷 큰수가 존재하지 않는 원소는 -1을 담습니다.
- */
+// 154539. 뒤에 있는 큰 수 찾기
 public class FindBigNum_154539 {
-
     public static void main(String[] args) {
         int[] numbers = {2, 3, 3, 5};
         FindBigNum_154539 q = new FindBigNum_154539();
         System.out.println(Arrays.toString(q.solution(numbers))); // [3, 5, 5, -1]
     }
+
     public int[] solution(int[] numbers) {
-        int[] answer = {};
+        int[] answer = new int[numbers.length];
+        Stack<Integer> stack = new Stack<>();
+        // 배열을 역순으로 순회하면서 각 원소에 대해 더 큰 수를 찾아 스택에 저장
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            // 스택의 top이 현재 수보다 크지 않으면 꺼내기
+            while (!stack.isEmpty() && stack.peek() <= numbers[i]) {
+                stack.pop();
+            }
 
+            if (stack.isEmpty()) { // 뒷 큰 수가 없다는 의미
+                answer[i] = -1;
+            } else { // 스택의 top이 현재 수의 뒷 큰 수
+                answer[i] = stack.peek();
+            }
 
+            // 현재수를 스택에 넣기
+            stack.push(numbers[i]);
+        }
+
+        return answer;
+    }
+
+    public int[] solution2(int[] numbers) {
+        int[] answer = new int[numbers.length];
+
+        int idx = 0;
+        for (int i = 0; i < numbers.length - 1; i++) {
+            answer[idx++] = getBigNum(numbers, i);
+        }
+
+        answer[answer.length - 1] = -1;
+
+        return answer;
+    }
+
+    public int getBigNum(int[] numbers, int index) {
+        int targetNum = numbers[index];
+        int answer = -1;
+        for (int i = index + 1; i < numbers.length; i++) {
+            if (targetNum < numbers[i]) {
+                answer = numbers[i];
+                break;
+            }
+        }
         return answer;
     }
 }
